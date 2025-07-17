e56dabcfd99a7bd408495196949c5e88a240fbc9
varoonsahgal <varoon.sahgal@gmail.com>
2025-07-17 13:48:44 -0700

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,8 @@
+variable "name" {
+  type        = string
+  description = "The name of the resource group"
+}
+variable "location" {
+type        = string
+description = "Azure region for the resource group"
+}
